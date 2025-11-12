61d308ac3cfce2e0989840b333a28100f35089e9
Akshay Yelle <02b3akshay@gmail.com>
2025-11-12 23:34:31 +0530

    setup - add postgres connection - add folder structure

--- fakturera-backend/index.js
+++ fakturera-backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import routes from "./routes/v1/index.js";
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
+app.use(express.json());
+
+app.use("/api/v1", routes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
